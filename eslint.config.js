8e24928b1154c0d2853ff8b1382e20c22bca7924
Javier Segovia <jsegoviadev@gmail.com>
2024-10-29 00:49:22 -0400

    Init commit

---
+++ eslint.config.js
@@ -0,0 +1,98 @@
+// @ts-check
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+// @ts-expect-error "importPlugin" is not typed
+import importPlugin from 'eslint-plugin-import'
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+
+  {
+    ignores: ['build/', 'node_modules/', 'worker-configuration.d.ts'],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  prettierRecommended,
+
+  reactPlugin.configs.flat?.recommended,
+  reactPlugin.configs.flat?.['jsx-runtime'],
+
+  {
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin,
+    },
+  },
+
+  {
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+  },
+
+  {
+    rules: {
+      'no-console': 'warn',
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+
+      // enforces consistent type specifier style for named imports
+      'import/consistent-type-specifier-style': 'error',
+      // disallow non-import statements appearing before import statements
+      'import/first': 'error',
+      // Require a newline after the last import/require in a group
+      'import/newline-after-import': 'error',
+      // Forbid import of modules using absolute paths
+      'import/no-absolute-path': 'error',
+      // disallow AMD require/define
+      'import/no-amd': 'error',
+
+      // disallow imports from duplicate paths
+      'import/no-duplicates': 'error',
+      // Forbid the use of extraneous packages
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+          peerDependencies: true,
+          optionalDependencies: false,
+        },
+      ],
+      // Forbid mutable exports
+      'import/no-mutable-exports': 'error',
+      // Prevent importing the default as if it were named
+      'import/no-named-default': 'error',
+      // Prohibit named exports
+      'import/no-named-export': 'off', // we want everything to be a named export
+      // Forbid a module from importing itself
+      'import/no-self-import': 'error',
+    },
+  },
+]
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import devServer, { defaultOptions } from '@hono/vite-dev-server'
+import adapter from '@hono/vite-dev-server/cloudflare'
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+declare module '@remix-run/cloudflare' {
+  interface Future {
+    v3_singleFetch: true
+  }
+}
+
+export default defineConfig({
+  build: {
+    minify: true,
+  },
+  ssr: {
+    resolve: {
+      externalConditions: ['workerd', 'worker'],
+    },
+  },
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+    devServer({
+      adapter,
+      entry: 'server.ts',
+      exclude: [...defaultOptions.exclude, '/assets/**', '/app/**'],
+      injectClientScript: false,
+    }),
+  ],
+})
